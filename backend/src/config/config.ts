2ad9cec393b9eb3ffba182d80484b4d8ac5f7139
nirajkr26 <nirajkumargupta2642006@gmail.com>
2026-03-14 21:48:50 +0530

    fix cookies

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -3,10 +3,18 @@ import { type CookieOptions } from 'express';
 /**
  * Global cookie configuration for JWT and sessions.
  */
+// export const cookieOptions: CookieOptions = {
+//   httpOnly: true, // Prevents client-side JS from accessing the cookie (XSS protection)
+//   secure: process.env.NODE_ENV === 'production', // Only sends over HTTPS in production
+//   sameSite: 'none', // Protects against CSRF while allowing standard navigation
+//   maxAge: 6 * 60 * 60 * 1000, // 6 hours
+//   path: '/', // Accessible across the entire domain
+// };
+
 export const cookieOptions: CookieOptions = {
   httpOnly: true, // Prevents client-side JS from accessing the cookie (XSS protection)
-  secure: process.env.NODE_ENV === 'production', // Only sends over HTTPS in production
-  sameSite: 'lax', // Protects against CSRF while allowing standard navigation
+  secure: true, // Only sends over HTTPS in production
+  sameSite: 'none', // Protects against CSRF while allowing standard navigation
   maxAge: 6 * 60 * 60 * 1000, // 6 hours
   path: '/', // Accessible across the entire domain
 };
